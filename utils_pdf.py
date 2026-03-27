e7c250aee88832fa5c3e98a5140f57bf8423eff4
joycebacaro-ship-it <joyce.bacaro@grupoacelerador.com.br>
2026-03-27 00:48:39 -0300

    Update utils_pdf.py

--- utils_pdf.py
+++ utils_pdf.py
@@ -66,7 +66,7 @@ def extrair_dados_pdf(caminho):
     # ----------------------------
     # DATA EMISSÃO (corrigido)
     # ----------------------------
-   data_emissao = ""
+data_emissao = ""
 
 match_data = re.search(
     r"Data da emissão da NFS-e\s*\n?\s*([0-9]{2}/[0-9]{2}/[0-9]{4})",
@@ -76,6 +76,7 @@ match_data = re.search(
 
 if match_data:
     data_emissao = match_data.group(1)
+    
     data_match = re.search(r"\d{2}/\d{2}/\d{4}", data_emissao)
     data_emissao = data_match.group(0) if data_match else ""
 
